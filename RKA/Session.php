5d61ead070cf4e3dc89929a03037d8532713911d
Rob Allen <rob@akrabat.com>
2015-05-24 22:45:36 +0100

    Improve Session::destroy()

--- RKA/Session.php
+++ RKA/Session.php
@@ -19,6 +19,21 @@ final class Session
 
     public static function destroy()
     {
+        $_SESSION = [];
+
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+            setcookie(
+                session_name(),
+                '',
+                time() - 42000,
+                $params["path"],
+                $params["domain"],
+                $params["secure"],
+                $params["httponly"]
+            );
+        }
+
         if (session_status() == PHP_SESSION_ACTIVE) {
             session_destroy();
         }
